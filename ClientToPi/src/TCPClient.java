23ad47a532b2fa2a188174306ca6601f40ee8d82
wstewart <william.stewart@furman.edu>
2016-05-26 15:04:06 -0400

    Initial commit of user client

---
+++ ClientToPi/src/TCPClient.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.net.*;
+
+public class TCPClient {
+
+	static boolean exit = false;
+	
+	public static void sendMessage() throws Exception{
+		String output;
+		String returnedString;
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		Socket clientSocket = new Socket("10.102.55.26",6789);
+		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		output = inFromUser.readLine();
+		if(output.toString().equals("exit")){
+			exit = true;
+		}
+		outToServer.writeBytes(output + '\n');
+		returnedString = inFromServer.readLine();
+		System.out.println("Server: " + returnedString);
+		clientSocket.close();
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try{
+			while(exit!=true){
+				sendMessage();
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
